4c6c6640a119ba83bad5ca6235c5a3ab22b3ea93
Azizcan Yalçın <azizcanyalcin@gmail.com>
2024-10-10 16:32:16 +0300

    activate doors refactored

--- Assets/Scripts/Utilities/ActivateDoors.cs
+++ Assets/Scripts/Utilities/ActivateDoors.cs
@@ -1,17 +1,21 @@
+using System.Linq;
 using UnityEngine;
 
 public class ActivateDoors : MonoBehaviour
 {
-    [SerializeField] private GameObject agent;
+    [SerializeField] private GameObject[] agent;
     private BoxCollider2D boxCollider;
 
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
     }
+
     private void Update()
     {
-        if (!agent) boxCollider.isTrigger = true;
-        else boxCollider.isTrigger = false;
+        if (agent.Where(a => a != null).Count() == 0)
+            boxCollider.isTrigger = true;
+        else
+            boxCollider.isTrigger = false;
     }
 }
