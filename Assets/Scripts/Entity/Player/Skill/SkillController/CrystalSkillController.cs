2dc57c61de1222e52b5b8321f20249c30cbc4053
Azizcan Yalçın <azizcanyalcin@gmail.com>
2024-10-22 20:25:30 +0300

    Removed unused packages

--- Assets/Scripts/Entity/Player/Skill/SkillController/CrystalSkillController.cs
+++ Assets/Scripts/Entity/Player/Skill/SkillController/CrystalSkillController.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Callbacks;
 using UnityEngine;
-using UnityEngine.Scripting.APIUpdating;
 
 public class CrystalSkillController : MonoBehaviour
 {
--- Assets/Scripts/Environment/InteractableObject.cs
+++ Assets/Scripts/Environment/InteractableObject.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using UnityEngine.UI;
 
